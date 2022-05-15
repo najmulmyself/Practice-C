bf2861c2132b4272fe553a76c1a833230eb23287
najmulmyself <najmul.myself@gmail.com>
2022-05-15 22:20:27 +0600

    pointer just points an address

--- Day 2/pointeragain.c
+++ Day 2/pointeragain.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int x  =10;
-    int *p;
+    // int x  =10;
+    // int *p;
 
-    printf("Value of x : %d\n",x);
+    // printf("Value of x : %d\n",x);
 
     // p = &x;
     // p = &x;
@@ -12,18 +12,32 @@ int main(){
     // printf("content of p is : %d\n",*p);
     // printf("content of p is : %d\n",p);
 
+    // p = &x;
+    // *p = 20;
+
+    // printf("Value of x : %d\n",x);
+
+    // x =15;
+
+    // printf("Value of x : %d\n",x);
+    // printf("Value stored at location %p is %d\n",p,*p);
+
+    // printf("Address  of x : %p\n",&x);
+    // printf("Value  of p : %p\n",p);
+
+    int x = 10 ,y;
+    int *p,*q;
+
     p = &x;
-    *p = 20;
+    y = *p;
+    *p = 15;
+    *q= 20;
 
     printf("Value of x : %d\n",x);
+    printf("Value of y : %d\n",y);
+    printf("Value of *p : %d\n",*p);
+    printf("Value of *q : %d\n",*q);
 
-    x =15;
-
-    printf("Value of x : %d\n",x);
-    printf("Value stored at location %p is %d\n",p,*p);
-
-    printf("Address  of x : %p\n",&x);
-    printf("Value  of p : %p\n",p);
 
     return 0;
 }
