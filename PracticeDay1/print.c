67fbee3a8e89dd29f540eeaecd927e3843e39857
najmulmyself <najmul.myself@gmail.com>
2022-10-18 20:33:30 +0600

    added % in c

---
+++ PracticeDay1/print.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("Hi there /\\.    I hope you are doing great and practicing well.\nNow this is a simple task you need to do.         Just care for the indentation.\nShow 100%% of sincerity.");
+    return 0;
+}
