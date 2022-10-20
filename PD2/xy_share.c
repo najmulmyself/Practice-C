e4aec5cf126efd20a754530123a498a24ee01962
najmulmyself <najmul.myself@gmail.com>
2022-10-21 01:16:41 +0600

    added more problem

---
+++ PD2/xy_share.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int X, Y, multiply;
+    scanf("%d%% %d%%", &X, &Y);
+    multiply = X * Y;
+    // printf("%d\n", X);
+    // printf("%d", Y);
+    printf("%d%%", multiply);
+    return 0;
+}
