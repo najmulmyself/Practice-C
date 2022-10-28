a64cf8b2b401431be9dc697934e80fb047581204
najmulmyself <najmul.myself@gmail.com>
2022-10-28 15:21:07 +0600

    added new soln

--- PD4/largestAndsmallest.c
+++ PD4/largestAndsmallest.c
@@ -5,8 +5,19 @@ int main()
 
     scanf("%d%d%d%d",&a,&b,&c,&d);
 
-    if(a>b){
-        
+    if(a>b && a>c && a>d){
+        printf("a is largest");
+    }
+    else if(b>a && b>c && b>d){
+        printf("b is largest");
+    }
+    
+    else if(c>a && c>b && c>d){
+        printf("c is largest");
+    }
+    
+    else if(d>a && d>b && d>c){
+        printf("d is largest");
     }
     return 0;
 }
