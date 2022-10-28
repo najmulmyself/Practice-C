32e5c270b0a5f7ad63304fa680950368b76c39ae
najmulmyself <najmul.myself@gmail.com>
2022-10-28 16:19:21 +0600

    fixed

--- PD4/largestAndsmallest.c
+++ PD4/largestAndsmallest.c
@@ -3,21 +3,81 @@ int main()
 {
     int a, b, c, d;
 
-    scanf("%d%d%d%d",&a,&b,&c,&d);
+    scanf("%d%d%d%d", &a, &b, &c, &d);
 
-    if(a>b && a>c && a>d){
-        printf("a is largest");
+    if (a > b && a > c && a > d)
+    {
+        if (b < c && b < d)
+        {
+            printf("Largest = %d\n", a);
+            printf("Smallest = %d\n", b);
+        }
+        else if (c < b && c < d)
+        {
+            printf("Largest = %d\n", a);
+            printf("Smallest = %d\n", c);
+        }
+        else
+        {
+            printf("Largest = %d\n", a);
+            printf("Smallest = %d\n", d);
+        }
     }
-    else if(b>a && b>c && b>d){
-        printf("b is largest");
+    else if (b > a && b > c && b > d)
+    {
+        if (a < c && a < d)
+        {
+            printf("Largest = %d\n", b);
+            printf("Smallest = %d\n", a);
+        }
+        else if (c < a && c < d)
+        {
+            printf("Largest = %d\n", b);
+            printf("Smallest = %d\n", c);
+        }
+        else
+        {
+            printf("Largest = %d\n", b);
+            printf("Smallest = %d\n", d);
+        }
     }
-    
-    else if(c>a && c>b && c>d){
-        printf("c is largest");
+
+    else if (c > a && c > b && c > d)
+    {
+        if (a < b && a < d)
+        {
+            printf("Largest = %d\n", c);
+            printf("Smallest = %d\n", a);
+        }
+        else if (b < a && b < d)
+        {
+            printf("Largest = %d\n", c);
+            printf("Smallest = %d\n", b);
+        }
+        else
+        {
+            printf("Largest = %d\n", c);
+            printf("Smallest = %d\n", d);
+        }
     }
-    
-    else if(d>a && d>b && d>c){
-        printf("d is largest");
+
+    else
+    {
+        if (a < b && a < c)
+        {
+            printf("Largest = %d\n", d);
+            printf("Smallest = %d\n", a);
+        }
+        else if (b < a && b < c)
+        {
+            printf("Largest = %d\n", d);
+            printf("Smallest = %d\n", b);
+        }
+        else
+        {
+            printf("Largest = %d\n", d);
+            printf("Smallest = %d\n", c);
+        }
     }
     return 0;
 }
