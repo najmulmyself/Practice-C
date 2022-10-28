871f38c69ed80f0464918e785460fd12eaaa6286
najmulmyself <najmul.myself@gmail.com>
2022-10-28 19:16:47 +0600

    added weird solved

--- PD4/weirdAlgo.c
+++ PD4/weirdAlgo.c
@@ -3,16 +3,18 @@ int main(){
     int n;
     scanf("%d",&n);
     while (n>1){
-        if(n%2 ==0){
-            printf("%d ,",n/2);
+
+         if(n%2 ==0){
+            printf("%d ,",n);
             n= n/2;
         }
         else{
             printf("%d, ",n);
             n = n-1;
-            printf("%d ",n);
         }
+
     }
+            printf("%d",n);
     
     
     return 0;
