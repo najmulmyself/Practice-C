d2dcdad28a7e09a126591cfffee95e821b9322fe
Najmul Huda <64252963+najmulmyself@users.noreply.github.com>
2022-10-30 06:43:26 +0000

    added from replit

---
+++ main.c
@@ -0,0 +1,49 @@
+// // #include <stdio.h>
+// // int main(){
+
+// //   int n,a,i=2;
+// //   scanf("%d",&n);
+// //    a = n/2;
+// //   if(n ==1){
+// //     printf("not prime");
+// //   }
+// //   else if(n==2){
+// //     printf("Prime");
+// //   }
+// //   // for( i  =2 ; i<=a;i++){
+// //   //   // if(n%i ==0){
+// //   //   //   printf("Composit");
+// //   //   //   break;
+// //   //   // }
+    
+// //   //     printf("Prime");
+// //   // }
+// //   while(i<=a){
+// //     if(n%i ==0){
+// //       printf("Composit");
+// //       break;
+// //     }
+// //     else if(){
+// //     printf("Prime")
+// //   }
+// //     i++;
+// //   }
+  
+// //   // else{
+// //   //     break;
+// //   //   }
+// //   return 0;
+// // }
+
+
+// #include <stdio.h>
+// int main(){
+//   int n;
+//   scanf("%d",&n);
+//   for(int i=2;i<n;i++){
+//     if(n%i == 0){
+      
+//     }
+//   }
+//   return 0;
+// }
