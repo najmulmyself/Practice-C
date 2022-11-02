e683fe3cd7823167687f9fa785dff8bed9100f55
najmulmyself <najmul.myself@gmail.com>
2022-11-02 23:49:07 +0600

    added new

--- codeforces/iq_test.c
+++ codeforces/iq_test.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 int main(){
-    int n,a[100];
+    int n,a[100],countEven=0,countOdd=0;
     scanf("%d",&n);
     for(int i= 0;i<n;i++){
         scanf("%d",&a[i]);
     }
+
+    for(int i =0;i<n;i++){
+        if(a[i]%2 == 0){
+            countEven++;
+        }
+        else if (a[i]%2 != 0)
+        {
+            countOdd++;
+        }
+        
+    }
+    if (countEven)
+    {
+        /* code */
+    }
     
+
 }
