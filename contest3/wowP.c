b7525186e7eb59ccfccb2ce0e4a541c340c384a7
najmulmyself <najmul.myself@gmail.com>
2022-12-17 13:50:13 +0600

    added new contest

---
+++ contest3/wowP.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void solved(int n){
+    
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    solved(n);
+    return 0;
+}
--- lab2/sortAgain.c
+++ lab2/sortAgain.c
@@ -17,3 +17,4 @@ int main(){
     puts(s);
     return 0;
 }
+
